23e19a3f5c0743757ee00ee065f48692a109b5f0
nicorapelas <nicorapelas@gmail.com>
2024-07-15 16:46:57 +0200

    first commit

---
+++ src/api/ngrok.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+const instance = axios.create({
+  baseURL: 'https://d739-105-184-170-54.ngrok-free.app',
+  // baseURL: 'https://cv-cloud-api.herokuapp.com',
+})
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem('token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (err) => {
+    return Promise.reject(err)
+  }
+)
+
+export default instance
