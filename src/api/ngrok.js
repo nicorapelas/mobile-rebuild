e6ddfa4a31ebdbbf4b016c31f994fe353ad234cc
nicorapelas <nicorapelas@gmail.com>
2024-10-07 16:57:53 +0200

    sorted burger menu

--- src/api/ngrok.js
+++ src/api/ngrok.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const instance = axios.create({
-  baseURL: 'https://2ff7-105-184-170-115.ngrok-free.app',
+  baseURL: 'https://cc9b-105-184-170-115.ngrok-free.app',
   // baseURL: 'https://cv-cloud-api.herokuapp.com',
 })
 
