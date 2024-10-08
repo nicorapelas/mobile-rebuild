cc39d62649e32a688a4a67411d2feb7655c5ba72
nicorapelas <nicorapelas@gmail.com>
2024-10-08 16:46:07 +0200

    ngrok error fix

--- src/api/ngrok.js
+++ src/api/ngrok.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const instance = axios.create({
-  baseURL: 'https://cc9b-105-184-170-115.ngrok-free.app',
+  baseURL: 'https://big-breads-reply.loca.lt',
   // baseURL: 'https://cv-cloud-api.herokuapp.com',
 })
 
